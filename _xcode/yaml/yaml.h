ef4fca44624c86daa3f17c280c1988a68d4dfccc
Andrew Karpushin <reven86@gmail.com>
2013-08-27 01:37:53 +0400

    added proper macosx and iOS targets

---
+++ _xcode/yaml/yaml.h
@@ -0,0 +1,13 @@
+//
+//  yaml.h
+//  yaml
+//
+//  Created by Andrew Karpushin on 11/17/12.
+//  Copyright (c) 2012 Andrew Karpushin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface yaml : NSObject
+
+@end
---
+++ _xcode/yaml/yaml.m
@@ -0,0 +1,13 @@
+//
+//  yaml.m
+//  yaml
+//
+//  Created by Andrew Karpushin on 11/17/12.
+//  Copyright (c) 2012 Andrew Karpushin. All rights reserved.
+//
+
+#import "yaml.h"
+
+@implementation yaml
+
+@end
